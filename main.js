5d73e71ccf03341431169887f6436e37a32f326a
Marissa Ross <mlyall.ross@ufl.edu>
2020-06-07 15:19:49 -0700

    pizza object, 3 properties, and 1 method

---
+++ main.js
@@ -0,0 +1,9 @@
+var pizza = {
+    crust: "New York",
+    cheese: "Goat",
+    sauce: "Olive Oil",
+    addToppings: function() {
+        console.log('Please add pepperoni and sausage');
+    }
+};
+
